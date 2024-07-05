255acffa4fd3c0c114bccef1d90f9cc75288f41d
Korchevvskyi Bohdan <msugenius@gmail.com>
2024-07-05 16:45:24 +0300

    test: update main for p2p staff testing

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/msugenius/file-storage/p2p"
 )
 
 func main() {
-	tr := p2p.NewTCPTransport(":3000")
+	tcpOpts := p2p.TCPTransportsOpts{
+		ListenAddr:    ":3000",
+		HandshakeFunc: p2p.DefaultHandshakeFunc,
+		Decoder:       p2p.DefaultDecoder{},
+		OnPeer:        p2p.DefaultOnPeer,
+	}
+
+	tr := p2p.NewTCPTransport(tcpOpts)
+
+	go func() {
+		for {
+			msg := <-tr.Consume()
+			fmt.Printf("%+v\n", msg)
+		}
+	}()
 
 	if err := tr.ListenAndAccept(); err != nil {
 		log.Fatal(err)
