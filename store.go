caaa43d7b2844909c304fa564d19facf0127c0b1
Korchevvskyi Bohdan <msugenius@gmail.com>
2024-07-05 17:02:12 +0300

    fix(store): add missing root dir to Has function

--- store.go
+++ store.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/sha1"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -85,9 +86,14 @@ func NewStore(opts StoreOpts) *Store {
 
 func (s *Store) Has(key string) bool {
 	pathKey := s.PathTransformFunc(key)
+	fullPathWithRoot := fmt.Sprintf("%s\\%s", s.Root, pathKey.FullPath())
 
-	_, err := os.Stat(pathKey.FullPath())
-	return err != os.ErrNotExist
+	_, err := os.Stat(fullPathWithRoot)
+	return !errors.Is(err, os.ErrNotExist)
+}
+
+func (s *Store) Clear() error {
+	return os.RemoveAll(s.Root)
 }
 
 func (s *Store) Delete(key string) error {
