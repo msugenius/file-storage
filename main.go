74016c7400b74a48df1a45bad52e7d0021a7c11b
Korchevvskyi Bohdan <msugenius@gmail.com>
2024-07-08 18:03:39 +0300

    feat(main): update for testing new functions

--- main.go
+++ main.go
@@ -1,35 +1,49 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"time"
 
 	"github.com/msugenius/file-storage/p2p"
 )
 
-func main() {
+func makeServer(listenAddr string, root string, nodes []string) *FileServer {
 	tcpTransportOpts := p2p.TCPTransportOpts{
-		ListenAddr:    ":3000",
+		ListenAddr:    listenAddr,
 		Decoder:       p2p.DefaultDecoder{},
 		HandshakeFunc: p2p.DefaultHandshakeFunc,
-		OnPeer:        p2p.DefaultOnPeer,
 	}
 	tcpTransport := p2p.NewTCPTransport(tcpTransportOpts)
 
 	fileServerOpts := FileServerOpts{
-		StorageRoot:       "data",
+		StorageRoot:       root,
 		PathTransformFunc: CASPathTransformFunc,
 		Transport:         tcpTransport,
+		BootstrapNodes:    nodes,
 	}
 
 	s := NewFileServer(fileServerOpts)
 
-	go func() {
-		time.Sleep(time.Second * 3)
-		s.Stop()
-	}()
+	tcpTransport.OnPeer = s.OnPeer
 
-	if err := s.Start(); err != nil {
-		log.Fatal(err)
+	return s
+}
+
+func main() {
+	s1 := makeServer(":3000", "data1", []string{})
+	s2 := makeServer(":4000", "data2", []string{":3000"})
+
+	go func() { log.Fatal(s1.Start()) }()
+	time.Sleep(1 * time.Second)
+	go func() { log.Fatal(s2.Start()) }()
+	time.Sleep(3 * time.Second)
+
+	data := bytes.NewReader([]byte("very very very large file"))
+	err := s2.StoreData("my-private-data", data)
+	if err != nil {
+		panic(err)
 	}
+
+	select {}
 }
